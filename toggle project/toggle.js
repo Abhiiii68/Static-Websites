3ff14ce29cebc7b2d3c6ef9e389c0be5a85a24d8
Abhishek Gupta <161931042+Abhiiii68@users.noreply.github.com>
2024-04-03 14:31:35 +0530

    Add files via upload

---
+++ toggle project/toggle.js
@@ -0,0 +1,12 @@
+function login() {
+    document.getElementById("Signup").style.left="470px";
+    document.getElementById('LoginForm').style.left="0px";
+    document.getElementById("Move").style.left="104px";
+}
+
+
+function signup() {
+    document.getElementById("Signup").style.left="0px";
+    document.getElementById('LoginForm').style.left="-500px";
+    document.getElementById("Move").style.left="216px";
+}
